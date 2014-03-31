250ca8f715e5e492d223079f5a3bcd1672a9bd09
Peter Fetterer <kb3gtn@gmail.com>
2014-03-31 01:19:10 -0400

    Moved driverlib (libdriver) and utils (libutils) over to cmake build enviroment. Dumped old Makefiles

---
+++ boards/EK-LM4F120LX/Common/driverlib/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -d "work" ]; then
+  echo "Cleaning up old build"
+  rm work -rf
+fi
+
+if [ $1 = "clean" ]; then
+    exit 0
+fi
+
+echo "Starting build from scratch.."
+
+mkdir work
+cd work
+cmake -G "CodeBlocks - Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=../toolchain-gcc-arm-none-eabi.cmake -DCMAKE_C_FLAGS="-nostartfiles" ../
+
+# for verbos builds
+#make VERBOSE=1
+
+#normal quite builds
+make
+
+cp libdriver.a ../
+
+# done with work..
+cd ..
+rm work -rf
+
+echo "build output in ./libdriver.a"
+
